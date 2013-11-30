590104cf49352f27ddb132067be9433181ceb50a
huellif <huellif@gmail.com>
2013-11-30 16:36:25 +0100

    added some swag
    - ported to native C++
    - added a little close dialog

--- CloseMaps.pro
+++ CloseMaps.pro
@@ -1,7 +1,6 @@
 symbian:{
 TARGET.UID3 = 0xE690E897
-LIBS += -lavkon \
-        -apgrfx.lib
+LIBS += -laknnotify -lcone
 TARGET.CAPABILITY += PowerMgmt
 vendorinfo = "%{\"huellifSoft\"}" ":\"huellifSoft\""
 my_deployment.pkg_prerules = vendorinfo
@@ -9,6 +8,8 @@ DEPLOYMENT += my_deployment
 DEPLOYMENT.display_name += CloseMaps
 VERSION = 1.1.0
 ICON = Icon.svg
+default_deployment.pkg_prerules -= pkg_depends_webkit
+default_deployment.pkg_prerules -= pkg_depends_qt
 }
 
 SOURCES += main.cpp
@@ -16,3 +17,8 @@ SOURCES += main.cpp
 
 include(deployment.pri)
 qtcAddDeployment()
+
+CONFIG -= qt
+
+INCLUDEPATH += C:\QtSDK\Symbian\SDKs\Symbian3Qt474\epoc32\include\mw\
+INCLUDEPATH += C:\QtSDK\Symbian\SDKs\Symbian3Qt474\epoc32\include\platform\
--- main.cpp
+++ main.cpp
@@ -1,65 +1,94 @@
-#include <apgtask.h>
-#include <eikenv.h>
-#include <akndiscreetpopup.h>
-#include <avkon.hrh>
+//popups
+#include <AknGlobalNote.h>
+#include <avkon.mbg>
 
-#include <QApplication>
+//UI:
+#include <coemain.h>
 
-int main(int argc, char *argv[])
+LOCAL_C void ExeMainL()
 {
-       QApplication app(argc, argv);
-       TRAP_IGNORE(CAknDiscreetPopup::ShowGlobalPopupL((_L("Success")), (_L("Closed Maps")),KAknsIIDNone, KNullDesC));
-       TFullName res;
-       RProcess ph;
+    CAknGlobalNote* note = CAknGlobalNote::NewLC();
+    note->SetGraphic(16584,16585);
+    TInt id = note->ShowNoteL(EAknGlobalPermanentNote, _L("Success.\nClosed all Nokia Maps related processes."));
 
-       TFindProcess find(_L("*[20029B6C]*"));//smart2go
-       while(find.Next(res) == KErrNone)
-       {
-           ph.Open(find);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFullName res;
+    RProcess ph;
 
-       TFindProcess find2(_L("*[20001F65]*"));//sg2variantsrv
-       while(find2.Next(res) == KErrNone)
-       {
-           ph.Open(find2);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFindProcess find(_L("*[20029B6C]*"));//smart2go
+    while(find.Next(res) == KErrNone)
+    {
+        ph.Open(find);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
 
-       TFindProcess find3(_L("*[20001F63]*"));//maps
-       while(find3.Next(res) == KErrNone)
-       {
-           ph.Open(find3);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFindProcess find2(_L("*[20001F65]*"));//sg2variantsrv
+    while(find2.Next(res) == KErrNone)
+    {
+        ph.Open(find2);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
 
-       TFindProcess find4(_L("*[20016BD0]*"));//syncshare
-       while(find4.Next(res) == KErrNone)
-       {
-           ph.Open(find4);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFindProcess find3(_L("*[20001F63]*"));//maps
+    while(find3.Next(res) == KErrNone)
+    {
+        ph.Open(find3);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
 
-       TFindProcess find5(_L("*[20029B69]*"));//drive
-       while(find5.Next(res) == KErrNone)
-       {
-           ph.Open(find5);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFindProcess find4(_L("*[20016BD0]*"));//syncshare
+    while(find4.Next(res) == KErrNone)
+    {
+        ph.Open(find4);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
 
-	  TFindProcess find6(_L("*[2001a983]*"));//odnpserver
-       while(find6.Next(res) == KErrNone)
-       {
-           ph.Open(find6);
-           ph.Kill(KErrNone);
-           ph.Close();
-       }
+    TFindProcess find5(_L("*[20029B69]*"));//drive
+    while(find5.Next(res) == KErrNone)
+    {
+        ph.Open(find5);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
 
-    return 1;
+   TFindProcess find6(_L("*[2001a983]*"));//odnpserver
+    while(find6.Next(res) == KErrNone)
+    {
+        ph.Open(find6);
+        ph.Kill(KErrNone);
+        ph.Close();
+    }
+
+    User::After(2000000);
+    note->CancelNoteL(id);
+    CleanupStack::PopAndDestroy(note);
+
 }
 
+TInt E32Main()
+{
+    CCoeEnv* coe = new CCoeEnv;
+    TRAPD(err, coe->ConstructL());
+
+    _LIT(KTxtLine1,"CloseMaps\n");
+    _LIT(KTxtLine2,"Do you want to close all processes which are related to Nokia Maps?\nThis will unlock compass for 3rd party apps and give you more free RAM & CPU time.");
+    _LIT(KButt1,"Kill");
+    _LIT(KButt2,"Cancel");
+    TInt whichbutton(-1);
+    TRequestStatus stat;
+    RNotifier notifier;
+    User::LeaveIfError(notifier.Connect());
+    notifier.Notify(KTxtLine1,KTxtLine2,KButt1,KButt2,whichbutton,stat);
+    User::WaitForRequest(stat);
+    if (whichbutton==0)
+    {
+        TRAPD(error, ExeMainL());
+    }
+    notifier.Close();
+
+    coe->DestroyEnvironment();
+    return 0;
+}
